7cbb05c90fec86b3f605f1d50633da79b2288a73
David Božjak <bozjak@gmail.com>
2025-12-03 16:24:22 +0100

    Added day 3

---
+++ Day_3/Program.cs
@@ -0,0 +1,48 @@
+﻿var inputLines = new StringInputProvider("Input.txt").ToList();
+
+Dictionary<(string, int), long> memcache = new();
+
+Console.WriteLine("Part 1:" + inputLines.Select(w => GetMaxValue(w, 2)).Sum());
+Console.WriteLine("Part 2:" + inputLines.Select(w => GetMaxValue(w, 12)).Sum());
+
+
+long GetMaxValue(string remainingInput, int remainingCharacters)
+{
+    if (!memcache.ContainsKey((remainingInput, remainingCharacters)))
+    {
+        memcache[(remainingInput, remainingCharacters)] = GetValues(remainingInput, remainingCharacters).Max();
+    }
+
+    return memcache[(remainingInput, remainingCharacters)];
+
+    IEnumerable<long> GetValues(string remainingInput, int remainingCharacters)
+    {
+        if (remainingCharacters == 0)
+        {
+            throw new Exception();
+        }
+        if (remainingInput.Length < remainingCharacters)
+        {
+            throw new Exception();
+        }
+        
+        if (remainingInput.Length == remainingCharacters)
+        {
+            yield return long.Parse(remainingInput);
+            yield break;
+        }
+
+        //don't include the current character
+        yield return GetMaxValue(remainingInput[1..], remainingCharacters);
+
+        //include the current character - either on it's own or in addition with the recursive step
+        if (remainingCharacters > 1)
+        {
+            yield return long.Parse(remainingInput[0] + GetMaxValue(remainingInput[1..], remainingCharacters - 1).ToString());
+        }
+        else
+        {
+            yield return long.Parse(remainingInput[0].ToString());
+        }
+    }
+}
